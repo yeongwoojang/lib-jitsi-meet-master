6d8366c5debc7079fb1a9a36ab008fa29c2060a7
jangyeongwoo <ywjang@digicaps.com>
2024-12-17 19:22:37 +0900

    좌표 리스너 위치 수정

--- modules/RTC/BridgeChannel.js
+++ modules/RTC/BridgeChannel.js
@@ -322,18 +322,18 @@ export default class BridgeChannel {
                 break;
             }
             case 'EndpointMessage': {
-                emitter.emit(RTCEvents.ENDPOINT_MESSAGE_RECEIVED, obj.from, obj.msgPayload);
-
-                break;
-            }
-            case 'EndpointStats': {
                 if (obj.type === 'CLICK_VIDEO_LAYOUT') {
                     console.log(obj.data.xPoint);
                     console.log(obj.data.yPoint);
                     emitter.emit(RTCEvents.X_Y_POINT_RECEIVED, obj.from, obj);
                 } else {
-                    emitter.emit(RTCEvents.ENDPOINT_STATS_RECEIVED, obj.from, obj);
+                    emitter.emit(RTCEvents.ENDPOINT_MESSAGE_RECEIVED, obj.from, obj.msgPayload);
                 }
+
+                break;
+            }
+            case 'EndpointStats': {
+                emitter.emit(RTCEvents.ENDPOINT_STATS_RECEIVED, obj.from, obj);
                 break;
             }
             case 'ForwardedSources': {
